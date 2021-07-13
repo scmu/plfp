185574e7fd26c832ce4b5bb930fdb27b7836919b
Shin-Cheng Mu <scm@iis.sinica.edu.tw>
2021-07-14 01:03:21 +0800

    [new] switching to Lanyon theme.

--- site.hs
+++ site.hs
@@ -2,72 +2,190 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
+import           Control.Monad (liftM)
+import           System.FilePath               (takeBaseName)
 
 
 --------------------------------------------------------------------------------
+main :: IO ()
+main = hakyllWith config $ do
+    mkAssets
+    mkCSS
+    mkError
+    mkPages
+    {- mkPosts; mkArchive; mkPaginate -}
+    mkIndex
+    mkTemplate
+    mkAtomXML
 
 config :: Configuration
 config = defaultConfiguration
   { destinationDirectory = "docs"
   }
 
-main :: IO ()
-main = hakyllWith config $ do
-    match "assets/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+mkAssets, mkCSS, mkError, mkPages, mkIndex,
+ mkTemplate, mkAtomXML :: Rules ()
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+mkAssets = match ("images/*" .||. "js/*") $ do
+      route   idRoute
+      compile copyFileCompiler
 
-    match "home/news.markdown" $ do
-        compile pandocCompiler
+mkCSS = match "css/*" $ do
+      route   idRoute
+      compile compressCssCompiler
 
-    match "menu/*" $ version "firstVer" $ do
-        route $ gsubRoute "menu/" (const "") `composeRoutes` setExtension "html"
-        compile pandocCompiler
+mkError = match "error/*" $ do
+      route $ (gsubRoute "error/" (const "") `composeRoutes` setExtension "html")
+      compile $ pandocCompiler
+          >>= applyAsTemplate siteCtx
+          >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> siteCtx)
 
-    match "templates/*" $ do
-        compile templateBodyCompiler
+mkPages = match "pages/*" $ do
+      route $ setExtension "html"
+      compile $ do
+          pageName <- takeBaseName . toFilePath <$> getUnderlying
+          let pageCtx = constField pageName "" `mappend`
+                        baseNodeCtx
+          let evalCtx = functionField "get-meta" getMetadataKey `mappend`
+                        functionField "eval" (evalCtxKey pageCtx)
+          let activeSidebarCtx = sidebarCtx (evalCtx <> pageCtx)
 
-    match "home/course-desc.markdown" $ do
-        route $ constRoute "index.html"
-        compile $ do
-            menu <- loadAll (hasVersion "firstVer") :: Compiler [Item String]
-            news <- loadBody "home/news.markdown" :: Compiler String
-            iden <- getUnderlying
-            let menuCtx =
-                    constField    "outpath" (toFilePath iden)     `mappend`
-                    functionField "active"  setActive             `mappend`
-                    defaultContext
-            let customCtx =
-                    field         "news"    (\x -> return news)   `mappend`
-                    listField     "menu"    menuCtx (return menu) `mappend`
-                    defaultContext
-            pandocCompiler
-                >>= loadAndApplyTemplate "templates/default.html" customCtx
-                >>= relativizeUrls
+          pandocCompiler
+              >>= saveSnapshot "page-content"
+              >>= loadAndApplyTemplate "templates/page.html"    siteCtx
+              >>= loadAndApplyTemplate "templates/default.html" (activeSidebarCtx <> siteCtx)
+              >>= relativizeUrls
 
-    match "menu/*" $ do
-        route $ gsubRoute "menu/" (const "") `composeRoutes` setExtension "html"
-        compile $ do
-            menu <- loadAll (hasVersion "firstVer") :: Compiler [Item String]
-            iden <- getUnderlying
-            let menuCtx =
-                    constField    "outpath" (toFilePath iden)     `mappend`
-                    functionField "active"  setActive             `mappend`
-                    defaultContext
-            let customCtx =
-                    listField     "menu"    menuCtx (return menu) `mappend`
-                    defaultContext
-            pandocCompiler
-                >>= loadAndApplyTemplate "templates/default.html" customCtx
-                >>= relativizeUrls
+mkIndex = match "home.markdown" $ do
+      route $ constRoute "index.html"
+      compile $ do
+          pageName <- takeBaseName . toFilePath <$> getUnderlying
+          let pageCtx = constField pageName "" `mappend`
+                        baseNodeCtx
+          let evalCtx = functionField "get-meta" getMetadataKey `mappend`
+                        functionField "eval" (evalCtxKey pageCtx)
+          let activeSidebarCtx = sidebarCtx (evalCtx <> pageCtx)
 
-setActive :: [String] -> Item String -> Compiler String
-setActive args (Item i x)
-    | path == outPath = return "class=\"active\""
-    | otherwise       = return ""
-    where path    = head args
-          outPath = last args
+          pandocCompiler
+              >>= saveSnapshot "page-content"
+              >>= loadAndApplyTemplate "templates/page.html"    siteCtx
+              >>= loadAndApplyTemplate "templates/default.html" (activeSidebarCtx <> siteCtx)
+              >>= relativizeUrls
+
+mkTemplate = match "templates/*" $ compile templateBodyCompiler
+
+mkAtomXML = create ["atom.xml"] $ do
+    route idRoute
+    compile $ do
+        let feedCtx = postCtx `mappend`
+                bodyField "description"
+        posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots "posts/*" "content"
+        renderAtom feedConfig feedCtx posts
+
+{-
+mkPosts = match "posts/*" $ do
+      route $ setExtension "html"
+      compile $ pandocCompiler
+          >>= saveSnapshot "content"
+          >>= loadAndApplyTemplate "templates/post.html"    postCtx
+          >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> siteCtx)
+          >>= relativizeUrls
+
+mkArchive = create ["archive.html"] $ do
+      route idRoute
+      compile $ do
+          posts <- recentFirst =<< loadAll "posts/*"
+          let archiveCtx =
+                  listField "posts" postCtx (return posts) `mappend`
+                  constField "title" "Archives"            `mappend`
+                  constField "archive" "" `mappend`
+                  siteCtx
+
+          makeItem ""
+              >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+              >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> archiveCtx)
+              >>= relativizeUrls
+
+mkPaginate = do
+  paginate <- buildPaginateWith postsGrouper "posts/*" postsPageId
+  paginateRules paginate $ \page pattern -> do
+    route idRoute
+    compile $ do
+        posts <- recentFirst =<< loadAllSnapshots pattern "content"
+        let indexCtx =
+                constField "title" (if page == 1 then "Home"
+                                                 else "Blog posts, page " ++ show page) `mappend`
+                listField "posts" postCtx (return posts) `mappend`
+                constField "home" "" `mappend`
+                paginateContext paginate page `mappend`
+                siteCtx
+
+        makeItem ""
+            >>= applyAsTemplate indexCtx
+            >>= loadAndApplyTemplate "templates/index.html" indexCtx
+            >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> indexCtx)
+            >>= relativizeUrls
+--------------------------------------------------------------------------------
+
+postsGrouper :: (MonadFail m, MonadMetadata m) => [Identifier] -> m [[Identifier]]
+postsGrouper = liftM (paginateEvery 3) . sortRecentFirst
+
+postsPageId :: PageNumber -> Identifier
+postsPageId n = fromFilePath $ if (n == 1) then "index.html" else show n ++ "/index.html"
+-}
+--------------------------------------------------------------------------------
+
+feedConfig :: FeedConfiguration
+feedConfig = FeedConfiguration
+    { feedTitle       = "Programming Languages: Functional Programming"
+    , feedDescription = "IM, NTU"
+    , feedAuthorName  = "Shin-Cheng Mu"
+    , feedAuthorEmail = "scm@iis.sinica.edu.tw"
+    , feedRoot        = "https://github.com/hahey/lanyon-hakyll"
+    }
+
+--------------------------------------------------------------------------------
+
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+siteCtx :: Context String
+siteCtx =
+    baseCtx `mappend`
+    constField "site_description"
+               "Programming Languages: Functional Programming" `mappend`
+    constField "site-url" "https://scmu.github.io/plfp/" `mappend`
+    constField "tagline" "National Taiwan University, 2020" `mappend`
+    constField "site-title" "程式語言：函數程式設計" `mappend`
+    constField "copy-year" "2020" `mappend`
+    constField "github-repo" "https://github.com/hahey/lanyon-hakyll" `mappend`
+    defaultContext
+
+baseCtx =
+    constField "baseurl" "http://localhost:8000"
+
+--------------------------------------------------------------------------------
+
+sidebarCtx :: Context String -> Context String
+sidebarCtx nodeCtx =
+    listField "list_pages" nodeCtx (loadAllSnapshots "pages/*" "page-content") `mappend`
+    defaultContext
+
+baseNodeCtx :: Context String
+baseNodeCtx =
+    urlField "node-url" `mappend`
+    titleField "title" `mappend`
+    baseCtx
+
+baseSidebarCtx = sidebarCtx baseNodeCtx
+
+evalCtxKey :: Context String -> [String] -> Item String -> Compiler String
+evalCtxKey context [key] item = (unContext context key [] item) >>= \cf ->
+        case cf of
+            StringField s -> return s
+            _             -> error $ "Internal error: StringField expected"
+
+getMetadataKey :: [String] -> Item String -> Compiler String
+getMetadataKey [key] item = getMetadataField' (itemIdentifier item) key
